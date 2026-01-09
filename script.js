987254d545a4661d63a26dc31a1e93fe36da8091
jaydenc390 <jaydenc390@nycstudents.net>
2026-01-09 12:26:22 -0500

    c

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let menuIcon = document.querySelector(".menu-icon");
-let addIcon = document.querySelector(".add-icon");
+let addIcon = document.querySelector(".adding-icon");
+let xIcon = document.querySelector(".x-icon");
+
 
 menuIcon.addEventListener("click", function () {
 
+});
+
+
+xIcon.addEventListener("click", function () {
+    menuIcon.style.display = "block";
 });
